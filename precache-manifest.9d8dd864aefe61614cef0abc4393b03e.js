c84fd4cd0e2b840689f96531ed6658fdeee2e5e7
Yogesh <yogeshkumaras0808@gmail.com>
2022-04-26 23:29:33 +0530

    Updates

---
+++ precache-manifest.9d8dd864aefe61614cef0abc4393b03e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e6d00d0cd8eeb44d328f8444518b02e",
+    "url": "/crud-application/index.html"
+  },
+  {
+    "revision": "bb06503e9beacc48c8b4",
+    "url": "/crud-application/static/css/main.1b3cb998.chunk.css"
+  },
+  {
+    "revision": "871a008f4680f912448b",
+    "url": "/crud-application/static/js/2.915a214a.chunk.js"
+  },
+  {
+    "revision": "fde8a65b8d307872c8c44f6de8ee07de",
+    "url": "/crud-application/static/js/2.915a214a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb06503e9beacc48c8b4",
+    "url": "/crud-application/static/js/main.ebb082c3.chunk.js"
+  },
+  {
+    "revision": "41ef298d73da0d660c52",
+    "url": "/crud-application/static/js/runtime-main.bfe94935.js"
+  }
+]);
